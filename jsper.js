f407c6ae9e63b3ea1cadd0d8d3366445a8e83a2e
rob <mcvey.rob@gmail.com>
2011-01-08 23:53:20 -0600

    made cookie engine less ugly and unreadable, added docs

--- jsper.js
+++ jsper.js
@@ -276,136 +276,155 @@ var jsper = (function(){
     }
 
     /**
-         *  Internal cookie storage engine
-         */
+     *  Internal cookie storage engine
+     */
     var cookieStorage = (function(){
         var window = this;
         var expiresAt = 30*24*60*60,
-        maxCookieSize = 4000,
+        max_cookie_size = 4000,
         prefix = "storageData_",
-        nameValueDelim = "::",
-        itemDelim = "++";
+        name_value_delimiter = "::",
+        item_delimiter = "++";
 
-        createCookie = function(name,value,expire) {
+        var create_cookie = function(name,value,expire) {
             var date = new Date();
             date.setTime(date.getTime() + expire);
             var expires = "; expires=" + date.toGMTString();
             document.cookie = name+"="+value+expires+"; path=/";
         };
-        readCookie = function(name) {
-            var nameEQ = name + "=", ca = document.cookie.split(';');
+        var read_cookie = function(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
             if(ca.length > 0){
-                for(var i=0,iLen=ca.length;i<iLen;i++) {
+                for( var i = 0, iLen = ca.length; i < iLen; i++ ) {
                     var c = ca[i];
-                    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+                    while (c.charAt(0) == ' ') {
+                        c = c.substring( 1, c.length );
+                    }
+                    if ( c.indexOf(nameEQ) === 0 ) {
+                        return c.substring( nameEQ.length, c.length );
+                    }
                 }
             }
             return null;
         };
-        eraseCookie = function(name) {
-            createCookie(name,"",-1);
+        var delete_cookie = function(name) {
+            create_cookie(name,"",-1);
         };
-        getAllCookieData = function(){
-            var data="",x=0;
+        var init_cookies = function(){
+            var data = "";
+            var x = 0;
             if(document.cookie !== "" && document.cookie !== null){
-                while(readCookie(prefix+x) !== null){
-                    data+=readCookie(prefix+x++);
+                while( read_cookie( prefix + x ) !== null ){
+                    var name = prefix + (x++);
+                    data += read_cookie( name );
                 }
             }else{
                 return [];
             }
             return data == "" ? [] : data.split('++');
         };
-        dataStringToCookies = function(data) {
-            var cookiesUsed = Math.ceil(data.length/maxCookieSize),
-            x=0;
-            while(x<cookiesUsed || readCookie(prefix+x) !== null){
-                if( x<cookiesUsed )
-                    createCookie(prefix+x, data.substr(x*maxCookieSize, ((x+1)*maxCookieSize>data.length ? data.length-x*maxCookieSize : maxCookieSize )), expiresAt);
-                else
-                    eraseCookie(prefix+x);
-                x++;
+        var data_string_to_cookies = function(data) {
+            var cookies_used = Math.ceil( data.length / max_cookie_size );
+            var index = 0;
+            while(index < cookies_used || read_cookie( prefix + index ) !== null){
+                var name = prefix + index;
+                if( index < cookies_used ){
+                    var start = index * max_cookie_size;
+                    var length = ((index + 1) * max_cookie_size) > data.length ? data.length - index * max_cookie_size : max_cookie_size;
+                    var value = data.substr( start, length );
+                    create_cookie( name, value, expiresAt);
+                } else {
+                    delete_cookie( name );
+                }
+                index++;
             }
         };
 
         return {
-            "length":0,
-            "setItem":function( key , value ){
-                var data=getAllCookieData(),x=0,exists=false,xlen=0;
+            length:0,
+            setItem:function( key , value ){
+                var data = init_cookies();
+                var exists = false;
+                var xlen = 0;
                 if(data !== null && _is_array(data)){
-                    xlen=data.length;
+                    xlen = data.length;
                 }
-                for(x=0;x<xlen;x++){
-                    var item = data[x].split(nameValueDelim);
+                for( var x=0; x < xlen; x++ ){
+                    var item = data[x].split( name_value_delimiter );
                     if( item[0] == key ){
                         item[1] = value;
-                        exists=true;
-                        data[x] = item.join(nameValueDelim);
-                        x=xlen;
+                        exists = true;
+                        data[x] = item.join( name_value_delimiter );
+                        x = xlen;
                     }
                 }
                 if( !exists ){
-                    data.push(key + nameValueDelim + value.replace(/::/g,": :").replace(/\+\+/g, "+ +") );
+                    data.push(key + name_value_delimiter + value.replace(/::/g,": :").replace(/\+\+/g, "+ +") );
                     this.keys.push(key);
-                    this["length"]++;
+                    this.length++;
                 }
 
-                dataStringToCookies( data.join(itemDelim) );
+                data_string_to_cookies( data.join( item_delimiter ) );
             },
-            "getItem":function( key ){
-                var data=getAllCookieData(),x=0,exists=false,xlen=0;
+            getItem:function( key ){
+                var data=init_cookies(),x=0,exists=false,xlen=0;
                 if(data !== null && _is_array(data)){
                     xlen=data.length;
                 }
                 for(x=0;x<xlen;x++){
-                    var item = data[x].split(nameValueDelim);
+                    var item = data[x].split(name_value_delimiter);
                     if( item[0] == key && !!item[1])
                         return item[1];
                 }
                 return null;
             },
-            "removeItem":function( key ){
-                var data=getAllCookieData(),x=0,xlen=data.length,exists=false,tempData=[];
-                for(x=0;x<xlen;x++){
-                    var item = data[x].split(nameValueDelim);
-                    if( item[0] != key )
-                        tempData.push(data[x]);
-                    else
-                        exists=true;
+            removeItem:function( key ){
+                var data = init_cookies();
+                var xlen = data.length;
+                var exists = false;
+                var temp = [];
+                for( var x = 0; x < xlen; x++ ){
+                    var item = data[x].split( name_value_delimiter );
+                    if( item[0] != key ) {
+                        temp.push( data[x] );
+                    } else {
+                        exists = true;
+                    }
                 }
-                if( !exists )
-                    return;
-
-                dataStringToCookies( tempData.join(itemDelim) );
-                this.setKeysAndLength();
+                if( !exists ){
+                    return null;
+                }
+                data_string_to_cookies( temp.join( item_delimiter ) );
+                return this.setKeysAndLength();
             },
 
-            "clear":function(){
+            clear:function(){
                 var x=0;
-                while(readCookie(prefix+x) !== null)
-                    eraseCookie(prefix+x++);
+                while(read_cookie(prefix+x) !== null)
+                    var name = prefix + (x++);
+                    delete_cookie( name );
                 this.keys = [];
-                this["length"] = 0;
+                this.length = 0;
             },
-            "key":function( key ){
-                return key<this["length"] ? this.keys[key] : null;
+            key:function( key ){
+                return key < this.length ? this.keys[key] : null;
             },
             keys: [],
             setKeysAndLength:function(){
                 if(document.cookie !== "" && document.cookie !== null){
-                    cookieStorage.keys = getAllCookieData();
+                    this.keys = init_cookies();
                 }else{
-                    cookieStorage["length"] = 0;
+                    this.length = 0;
                     return null;
                 }
                 var x=0;
-                if(cookieStorage.keys.length !== 0){
-                    while( cookieStorage.keys[x] ){
-                        cookieStorage.keys[x] = cookieStorage.keys[x++].split("::")[0];
+                if(this.keys.length !== 0){
+                    while( this.keys[x] ){
+                        this.keys[x] = this.keys[x++].split("::")[0];
                     }
                 }
-                cookieStorage["length"] = cookieStorage.keys.length;
+                this.length = this.keys.length;
                 return null;
             },
             onstorage:function(e){
@@ -462,6 +481,17 @@ var jsper = (function(){
         engines:['localStorage', 'cookie', 'sessionStorage'],
         storage:(supports.html5_storage()) ? localStorage : cookieStorage,
         storage_engine:'localStorage',
+        /**
+         * this should be used instead of force_engine or storage_engine
+         * if no parameter is passed, the engine is returned; if one is passed the engine is changed to that
+         */
+        engine:function( eng ){
+            if( eng !== undefined ){
+                this.force_engine( eng );
+                return this.storage_engine;
+            }
+            return this.storage_engine;
+        },
         support:false,
         /**
          * valid values to force_engine are: cookie, cookies, localStorage, local, localstorage, session, sessionstorage, sessionStorage
